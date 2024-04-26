66eb2b724bfaf418cd707198f8828e93cb93683d
Souvik Das <souvikdas.dev@gmail.com>
2024-04-26 18:37:01 +0530

    Added login screen

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const login_cartoon = require("./assets/login_cartoon.png");
+
+  const [text, onChangeText] = React.useState('Useless Text');
+  const [number, onChangeNumber] = React.useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <Image source={login_cartoon} style={{ width: 245, height: 245, alignSelf: 'center' }} />
+
+        <Text style={{ textAlign: 'center' }}>Don't have any account?
+          <Text style={{ fontWeight: '600', textDecorationLine: 'underline' }}> Sign Up </Text>
+        </Text>
+
+        <View style={{
+          margin: 25,
+        }}>
+          <Text style={{ fontWeight: '800', fontSize: 28 }}>Login</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +44,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f97316'
+  },
+  input: {
+    height: 40,
+    marginVertical: 10,
+    borderWidth: 1,
+    padding: 10,
   },
 });
